9af09ffe099224f91faf13f88edf39abd5f6b883
andresgs03 <andresitogs0226@gmail.com>
2026-04-27 10:34:35 -0500

    Add methods to retrieve nombre and ticker

--- accion.py
+++ accion.py
@@ -1,4 +1,10 @@
-Class Accion:
-def __init__(self, nombre, ticker):
+class Accion:
+    def __init__(self, nombre, ticker):
         self.nombre = nombre
         self.ticker = ticker
+
+    def obtener_nombre(self):
+        return self.nombre
+
+    def obtener_ticker(self):
+        return self.ticker
