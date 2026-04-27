08cd81d6ad14783a18f569d9091db1ff3c9d5794
andresgs03 <andresitogs0226@gmail.com>
2026-04-27 10:55:10 -0500

    Enhance PosicionAccion class with new methods

--- posicion_accion.py
+++ posicion_accion.py
@@ -1,3 +1,44 @@
 class PosicionAccion:
-def __init__(self, accion):
+    def __init__(self, accion):
         self.accion = accion
+        self.cantidad = 0
+        self.costo_total = 0.0
+        self.dividendos_acumulados = 0.0
+
+    def comprar(self, cantidad, costo):
+        self.cantidad += cantidad
+        self.costo_total += costo
+
+    def vender(self, cantidad):
+        if cantidad > self.cantidad or cantidad <= 0:
+            return False
+
+        if self.cantidad > 0:
+            costo_promedio = self.costo_total / self.cantidad
+            self.costo_total -= costo_promedio * cantidad
+
+        self.cantidad -= cantidad
+
+        if self.cantidad == 0:
+            self.costo_total = 0.0
+
+        return True
+
+    def agregar_dividendo(self, valor):
+        self.dividendos_acumulados += valor
+
+    def valor_actual(self, precio_cierre):
+        return self.cantidad * precio_cierre
+
+    def rentabilidad(self, precio_cierre):
+        if self.costo_total <= 0:
+            return 0.0
+
+        valor = self.valor_actual(precio_cierre)
+        return ((valor + self.dividendos_acumulados - self.costo_total) / self.costo_total) * 100
+
+    def obtener_cantidad(self):
+        return self.cantidad
+
+    def obtener_costo_total(self):
+        return self.costo_total
