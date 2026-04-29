f13673733abadd100b0381dc4acb8a62a707f3d8
andresgs03 <andresitogs0226@gmail.com>
2026-04-29 18:20:35 -0500

    Add type hints to PosicionAccion methods

--- posicion_accion.py
+++ posicion_accion.py
@@ -1,15 +1,15 @@
 class PosicionAccion:
-    def __init__(self, accion):
+    def __init__(self, accion) -> None:
         self.accion = accion
         self.cantidad = 0
         self.costo_total = 0.0
         self.dividendos_acumulados = 0.0
 
-    def comprar(self, cantidad, costo):
+    def comprar(self, cantidad: int, costo: float) -> None:
         self.cantidad += cantidad
         self.costo_total += costo
-        
-    def vender(self, cantidad):
+
+    def vender(self, cantidad: int) -> tuple[bool, float]:
         if cantidad > self.cantidad or cantidad <= 0:
             return False, 0.0
 
@@ -27,21 +27,21 @@ class PosicionAccion:
 
         return True, costo_retirado
 
-    def agregar_dividendo(self, valor):
+    def agregar_dividendo(self, valor: float) -> None:
         self.dividendos_acumulados += valor
 
-    def valor_actual(self, precio_cierre):
+    def valor_actual(self, precio_cierre: float) -> float:
         return self.cantidad * precio_cierre
 
-    def rentabilidad(self, precio_cierre):
+    def rentabilidad(self, precio_cierre: float) -> float:
         if self.costo_total <= 0:
             return 0.0
 
         valor = self.valor_actual(precio_cierre)
         return ((valor + self.dividendos_acumulados - self.costo_total) / self.costo_total) * 100
 
-    def obtener_cantidad(self):
+    def obtener_cantidad(self) -> int:
         return self.cantidad
 
-    def obtener_costo_total(self):
+    def obtener_costo_total(self) -> float:
         return self.costo_total
